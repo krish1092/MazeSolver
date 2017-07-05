c1f07660137a36ccee73810767926b2a8de64403
Krishnan Subramanian <krish1610@gmail.com>
2017-07-05 06:55:30 -0500

    First Commit of the Spring Boot - React application

---
+++ MazeSolver/src/test/java/com/krish/peoplenet/maze/MazeSolverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krish.peoplenet.maze;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MazeSolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
